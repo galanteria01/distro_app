1044eb16aa69ab29d51a891ca052858b2e8ebf3a
Shoury Sharma <shanuu12e@gmail.com>
2021-02-06 11:49:24 +0530

    Working model

--- lib/activity/DistroDetails.dart
+++ lib/activity/DistroDetails.dart
@@ -28,13 +28,59 @@ class _DistroDetailsState extends State<DistroDetails> {
             Padding(
               padding: const EdgeInsets.all(18.0),
               child: Container(
-                child: Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold
+                    ),
                   ),
+                   CircleAvatar(
+                     backgroundImage: AssetImage(imagePath),
+                     maxRadius: 24.0,
+                     backgroundColor: Colors.white60,
+                   )
+                ],
                 ),
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                hugeInfo,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400
+                ),
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton.icon(
+                  onPressed: () {},
+                  label: Text("Download"),
+                  icon: Icon(Icons.download_rounded),
+                ),
+                TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.web),
+                    label: Text("Website")
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextButton.icon(
+              label: Text("Installation method"),
+              icon: Icon(Icons.add_to_queue),
+              onPressed: () {},
+              style: ButtonStyle(
               ),
             )
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,20 @@ import 'activity/Developer.dart';
 import 'activity/DistroDetails.dart';
 import 'activity/DistroList.dart';
 import 'activity/Loading.dart';
+import 'activity/Settings.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(fontFamily: 'OpenSans'),
+    theme: ThemeData(
+        fontFamily: 'OpenSans',
+    ),
     initialRoute: '/loading',
     routes: {
       '/':(context) => DistroList(),
       '/loading':(context) => Loading(),
       '/distro':(context) => DistroDetails(),
       '/developer':(context) => Developer(),
+      '/settings':(context) => Settings(),
     },
   ));
 }
